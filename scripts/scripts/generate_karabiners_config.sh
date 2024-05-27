a6d48bdc7982ddb82d414a485d7f2f8d75d3a8e0
0xAquaWolf <0xaquawolf@protonmail.com>
2024-05-27 18:39:57 +0700

    renamed karabiners script

--- scripts/scripts/generate_karabiners_config.sh
+++ scripts/scripts/generate_karabiners_config.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 # this script is going to be used to update karabiners json
+
 # CD into karabiners repo dir
 pushd ~/repos/karabiner/
-# this script will build the file
+
 echo "Building Karabiners.json config file"
-yarn run build
-# then it will delete an old file
-pushd ~/.config/karabiner/
+bun run build
+
 echo "Deleting old karabiner.json config file"
+pushd ~/.config/karabiner/
 rm -rf karabiner.json
+
 # stow the new files
 pushd ~/AquaFiles/
 echo "Stowing Karabiners"
 stow karabiner
 
 echo "Removing all directories from stack popd"
-# Pop all directories off of the stack
 popd
 popd
 
 echo "Restarting karabiner service"
-# restart the karabiners service
 launchctl kickstart -k gui/$(id -u)/org.pqrs.karabiner.karabiner_console_user_server
