f6cb673473d16816ebc81cfe7472037d4c2ce2b0
0xAquaWolf <0xaquawolf@protonmail.com>
2024-05-27 16:39:42 +0700

    added sketchybar install script

---
+++ install_sketchybar.sh
@@ -0,0 +1,25 @@
+echo "Installing Dependencies"
+# Packages
+brew install lua
+brew install switchaudio-osx
+brew install nowplaying-cli
+
+brew tap FelixKratz/formulae
+brew install sketchybar
+
+# Fonts
+brew install --cask sf-symbols
+brew install --cask homebrew/cask-fonts/font-sf-mono
+brew install --cask homebrew/cask-fonts/font-sf-pro
+
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.5/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+
+# SbarLua
+(git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
+
+echo "Cloning Config"
+git clone https://github.com/FelixKratz/dotfiles.git /tmp/dotfiles
+mv $HOME/.config/sketchybar $HOME/.config/sketchybar_backup
+mv /tmp/dotfiles/.config/sketchybar $HOME/.config/sketchybar
+rm -rf /tmp/dotfiles
+brew services restart sketchybar
